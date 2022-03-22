3f79536c7c276b254978f7f9a45b68896ae7d0a8
trololol91 <richmond998999@yahoo.com>
2022-03-21 19:39:52 -0700

    Create sample.ts to parse CIBC transactions

--- src/sample.ts
+++ src/sample.ts
@@ -1,5 +1,26 @@
+import transactions from '../resource/transactions.json';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+
 async function main() {
-  console.log('Sample');
+  let fileOutput = '';
+  transactions.transactions.forEach((transaction) => {
+    if (transaction.debit) {
+      const description = transaction.transactionDescription.replace(',', '');
+      const date = new Date(transaction.date)
+        .toISOString()
+        .matchAll(/[0-9]+-[0-9]+-[0-9]+/g);
+
+      fileOutput += `${description}, ${date.next().value[0]}, ${
+        transaction.debit
+      }\n`;
+    }
+  });
+
+  if (!existsSync('dist')) {
+    mkdirSync('dist');
+  }
+
+  writeFileSync('dist/output.csv', fileOutput);
 }
 
 main().then((_) => _);
